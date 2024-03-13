3e25b2dd6bc414c3935f30e50887cb515c8dbeca
Natan Miranda <natanmiramda@gmail.com>
2024-03-12 21:07:37 -0300

    resolvido

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -3,14 +3,24 @@ function carregar(){
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
+    //var hora = 19
     msg.innerHTML = `Agora são ${hora} Horas.`
-    if ( hora >= 0 && hora < 12 ){
-        img.src = 'manhap.png'}
-    else if ( hora >= 12 && hora <= 18 ){
+
+    if (hora >= 0 && hora < 12){
+
+        img.src = 'manhap.png'
+        document.body.style.background = '#e2cd91'
+
+    } else if (hora >= 12 && hora <= 18 ){
+
         img.src = 'tardep.png'
-    }
-    else{
+        document.body.style.background = '#b9846f'
+
+    } else {
+
         img.src = 'noitep.png'
+        document.body.style.background = '#515154'
+
     }
 
 
