10d5389b536362037c70d228a5ac10b192ef2c75
Natan Miranda <natanmiramda@gmail.com>
2024-04-08 22:56:17 -0300

    fim da aula

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -4,17 +4,20 @@ function contar() {
   let passo = document.getElementById('txtp')
   let res = document.getElementById('res')
 
-  if (ini.ariaValueMax.length == 0 || fim.ariaValueMax.length == 0 || passo.ariaValueMax.length == 0) {
+  if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
     window.alert('[ERRO] faltam dado!')
   } else{
     res.innerHTML= 'Contando: '
     let i = Number(ini.value)
     let f = Number(fim.value)
     let p = Number(passo.value)
-
+    if (p <= 0 ){
+      window.alert('Passo invalido! Considerando PASSO 1')
+      p = 1
+    }
     for(let c = i; c <= f; c += p) {
-        res.innerHTML += '${c}'
+       res.innerHTML += `${c} `
     }
   }
 
-}
+} 
